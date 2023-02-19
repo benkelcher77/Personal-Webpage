1ffe0505ef7d49d01c69dfe54403ede149ebd02c
Ben Kelcher <benkelcher77@gmail.com>
2023-02-19 11:43:02 -0500

    Added OAuth to Github API request header.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = express()
 
 const https = require('node:https');
 
+require('dotenv').config()
+
 app.get("/git", (req, res) => {
     https_request = https.get({
         hostname: 'api.github.com',
@@ -10,12 +12,14 @@ app.get("/git", (req, res) => {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
         },
+        OAUth: process.env.GIT_OAUTH
     }, (https_response) => {
         https_response.pipe(res)
     }).on('error', (e) => {
         console.log(e);
         res.status(500).send("Something went wrong!");
     });
+
 });
 
 app.get("/api", (req, res) => {
